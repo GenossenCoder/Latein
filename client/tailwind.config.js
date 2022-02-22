bb8702b27edd1c1baadc12e1edeb128fc612b784
GenossenCoder <80360252+GenossenCoder@users.noreply.github.com>
2022-02-22 22:01:05 +0100

    tailwind

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -3,6 +3,19 @@ module.exports = {
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
+    screens: {
+      '2xl': {'max': '2555px'},
+
+      'xl': {'max': '1850px'},
+
+      'lg': {'max': '1300px'},
+
+      'md': {'max': '770px'},
+
+      'sm': {'max': '639px'},
+
+      'xs': {'max': '350'},
+    },
     extend: {},
   },
   plugins: [],
