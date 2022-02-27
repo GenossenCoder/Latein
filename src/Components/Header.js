f7e1ffa8990fd8dec91b1d45c8c0463e771c615a
GenossenCoder <80360252+GenossenCoder@users.noreply.github.com>
2022-02-27 20:55:41 +0100

    wer zur hölle denkt sich denn alt porps aus

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -4,7 +4,7 @@ import Logo from '../Images/Logo.svg'
 const Header = () => {
   return (
     <div className="flex items-center justify-center text font-medium p-2 bg-emerald-400  shadow-sm">
-        <img className="w-8" src={Logo}/>
+        <img className="w-8" src={Logo} alt=""/>
         <h1 className="text text-7xl p-2">Die Giganten </h1>
     </div>
   )
