2d78d2dbd414eb4794d6e27021fa945dbda76da1
GenossenCoder <80360252+GenossenCoder@users.noreply.github.com>
2022-03-02 21:25:18 +0100

    final version wenn das noch mal jemand anfasst wird er erschlagen von Ovid höchst persönlich

--- src/Components/Summary.js
+++ src/Components/Summary.js
@@ -2,23 +2,31 @@ import React from 'react'
 import SummaryItem from './SummaryItem'
 import GigantomachieImg from '../Images/Gigantomachie.jpg'
 import OvidImg from '../Images/Ovid.jpeg'
+import GeschichteImg from '../Images/Geschichte.jpg'
 
+const Geschichte = `
+Die Gigantomachie bzw. die Geschichte der Giganten wird besonders in der Kunst immer wieder aufgegriffen.
+So lassen sich schon ab dem Mittelalter Kunstwerke finden, die diese Metamorphose als Motiv für Gemälde verwenden. Im Mittelalter
+wurden die Giganten aber als übergroße Menschen dargestellt während sie in der Mythologie als Mischwesen zwischen Schlange
+und Mensch beschrieben wurden
+
+`
 const Gigantomachie = `
-  	Die Gigantomachie beschreibt den Kampf der Götter des Olymps gegen die Giganten.
-    Als Reaktion auf den Sieg der Götter über die Titanen beauftragte Gaia ihre Kinder,
-    die Giganten, die olympischen Götter zu stürtzen.
-    Als dann die Giganten gegen die olympischen Götter kämpften nahm der Kampf eine unerwartete Wendung,
-    denn jedes mal, wenn ein Gigant zu Boden ging stand er mit neuer Kraft wieder auf.
-  	Schließlich befragten die Götter ein Orakel, welches ihnen vorraussagte, dass die Giganten nur mit Hilfe der
-    Sterblichen besiegt werden können. Und so gelang es den Göttern schließlich mit Hilfe von Herkules die Giganten
-    entgültigt zu vernichten.
+Die Gigantomachie beschreibt den Kampf der Götter des Olymps gegen die Giganten.
+Als Reaktion auf den Sieg der Götter über die Titanen beauftragte Gaia ihre Kinder,
+die Giganten, die olympischen Götter zu stürzen.
+Als dann die Giganten gegen die olympischen Götter kämpften nahm der Kampf eine unerwartete Wendung.
+Jedes Mal, wenn ein Gigant zu Boden ging, stand er mit neuer Kraft wieder auf.
+Schließlich befragten die Götter ein Orakel, welches ihnen voraussagte, dass die Giganten nur mit Hilfe der
+Sterblichen besiegt werden können. So gelang es den Göttern schließlich mit Hilfe von Herkules und Dionysos die Giganten
+endgültig zu vernichten.
+
 `
 const Ovid = `
-    Bei Ovid wird gerade der Kampf der Titanen verkürtzt dargestellt, lediglich eine kurze Beschreibung, wie die 
-    Giganten vom Berg Olymp geschleudert werden, lässt sich in Ovid's Versen finden. Viel mehr liegt der Fokus auf
-    dem was aus den Giganten wird. Bei Ovid erschafft Gaia aus den Überresten der Giganten Menschen um an das Erbe ihrer 
-    Kinder zu erinnern. Diese neuerschaffende Menschen und deren nachkommen waren gekennzeichnet durch ihren Hass auf die Götter.
-
+In Ovid's Versen wird gerade der Kampf der Titanen verkürzt dargestellt. Lediglich eine kurze Beschreibung, wie die 
+Giganten vom Berg Olymp geschleudert werden, lässt sich in Ovid's Versen finden. Vielmehr liegt der Fokus auf
+dem was aus den Giganten wird. Bei Ovid erschafft Gaia aus den Überresten der Giganten Menschen, um an das Erbe ihrer 
+Kinder zu erinnern. Diese neuerschaffenen Menschen und deren Nachkommen waren gekennzeichnet durch ihren Hass auf die Götter.
 `
 
 const Summary = () => {
@@ -26,7 +34,7 @@ const Summary = () => {
     <div className="flex flex-col items-center justify-center">
         <SummaryItem num="1" title="Die Gigantomachie" text={Gigantomachie} img={GigantomachieImg}/>
         <SummaryItem num="2" title="Die Giganten bei Ovid" text={Ovid} img={OvidImg}/>
-        <SummaryItem num="3"/>
+        <SummaryItem num="3" title="Die Giganten im Verlauf der Geschichte" text={Geschichte} img={GeschichteImg}/>
     </div>
   )
 }
